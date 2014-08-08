8bef916bea7f92049169bf49facd29f71fd8d845
Warren Richards <wjrichards.da@gmail.com>
2014-08-08 01:06:15 -0400

    initial commit

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -8,7 +8,7 @@ echo '#-------------------------------------------#'
 echo '#-------------------------------------------#'
 sleep 2
 
-read -p 'Press [Enter] Key To Start'
+read -p 'Press [Enter] To Start'
 sleep 1
 
 echo  'This May Take A While...Grab a Coffee & Relax!'
@@ -30,14 +30,23 @@ echo '------Installing RPM Fusion------'
 sleep 2
 sudo yum localinstall --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
+# ENABLING MP3 AND DVD PLAYBACKS
+echo '------Installing Media Codecs------'
+sleep 2
+sudo yum install gstreamer-plugins-bad gstreamer-plugins-bad-free-extras gstreamer-plugins-bad-nonfree gstreamer-plugins-ugly gstreamer-ffmpeg ffmpeg mencoder ffmpeg2theora mplayer libdvdcss
+
+
+# INSTALLING FAVOURITE APPS
+echo '------Installing A LIST Of MY FAVOURITE APPS------'
+sleep 2
+sudo yum install chrome 
 
 # CLEANING YUM CACHE
-echo '------Almost Finished...Cleaning Apt Cache------'
+echo '------Almost Done, Cleaning Yum Cache------'
 sleep 2
 sudo yum clean all
 
 sleep 2
 echo '#------FINISHED------#'
-echo -e '\033[0m'
 
 exit $?
