b6086360073d36418ba4b288232390cc71b1606b
Warren Richards <wjrichards.da@gmail.com>
2015-02-26 11:16:44 -0500

    initial commit

--- scripts/sass-install.sh
+++ scripts/sass-install.sh
@@ -6,3 +6,24 @@ echo '#            Installing SASS                #'
 echo '#-------------------------------------------#'
 echo '#-------------------------------------------#'
 sleep 2
+
+read -p 'Press [Enter] Key To Start'
+sleep 2
+
+# Installing Dependencies
+echo '-------Installing Ruby and other Dependenties------'
+sudo dnf install ruby nodejs git
+sleep 2
+
+# Installing Sass & Others
+echo '-------Installing Sass------'
+gem install sass
+sleep 2
+
+echo '-------Installing Compass------'
+gem install compass
+sleep 2
+
+echo '-------Installing foundation------'
+gem install foundation
+sleep 2
